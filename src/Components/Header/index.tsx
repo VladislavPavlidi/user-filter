5f1acfe33f3127bcfda36883b4452ed100bda1df
VladPavlidi <vladislavpavlidi25@gmail.com>
2022-07-30 21:09:32 +0300

    add material

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import ROUTES from '../../routes'
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import { makeStyles } from "@mui/material/styles";
+
+// const useStyles = makeStyles(() => ({
+//   appBar: {
+//     width: '100%',
+//     padding: 1,
+//   },
+// }));
 
 export default function Header() {
   return (
-    <header>
-        <nav>
-            <Link to={ROUTES.home()}>На главную</Link>
-            <Link to={ROUTES.users()}>Пользователи</Link>
-            <Link to={ROUTES.profile()}>Профиль</Link>
-        </nav>
-    </header>
+    <AppBar sx={{
+      padding: 1,
+      width: '100%',
+   }}>
+      <Box
+       sx={{
+          padding: 1,
+          width: '100%',
+          height: 20,
+       }}
+       >
+        <Link color="" href={ROUTES.home()}>На главную</Link>
+        <Link color="white" href={ROUTES.users()}>Пользователи</Link>
+        <Link color="white" href={ROUTES.profile()}>Профиль</Link>
+      </Box>
+    </AppBar>
   )
 }
