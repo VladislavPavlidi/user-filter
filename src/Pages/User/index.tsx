772a1315162372f407d2ba868b6afab31ca33643
VladPavlidi <vladislavpavlidi25@gmail.com>
2022-08-02 20:47:37 +0300

    add user page content

--- src/Pages/User/index.tsx
+++ src/Pages/User/index.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { Avatar, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Api from "../../Api";
+import { IUserCard } from "../Users/Card";
 
 export default function User() {
-  return <div>User</div>;
+  const { id } = useParams();
+  const [user, setUser] = useState<IUserCard | null>(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await Api.get(`https://reqres.in/api/users/${id}`);
+        setUser(response?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
+  if (!user) return null;
+
+  return (
+    <Grid
+      sx={{ flexDirection: "column" }}
+      container
+      gap={5}
+      alignItems="center"
+    >
+      <Avatar
+        alt="avatar"
+        src={user?.avatar}
+        sx={{ width: 100, height: 100 }}
+      />
+      <Typography gutterBottom variant="h5" component="div">
+        {user?.first_name} {user?.last_name}
+      </Typography>
+      <Typography variant="h5" color="text.secondary">
+        {user?.email}
+      </Typography>
+    </Grid>
+  );
 }
